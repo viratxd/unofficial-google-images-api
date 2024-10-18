003c5c9ef10bc1a9b70504f812326452f0e1702c
Manvir Singh <manvir.singh@idcman.ae>
2024-10-18 11:07:02 +0400

    basic logging added

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import config from './config';
 import router from './router';
 import NotFound from './controllers/notfound';
 import { createSuccessResponse } from 'utils/response';
+import { logResponse } from 'utils/logging';
 
 const app = express();
 app.use(json());
+app.use(logResponse)
 app.use('/api', router)
 app.get('/', (req: Request, res: Response) => {
   createSuccessResponse(res, 'Server is up and running.')  
